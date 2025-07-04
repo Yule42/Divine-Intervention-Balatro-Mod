cfb10b0f64ffec029fb7231b6b80855faaca9c64
Yule42 <gamesandstuff4ever@gmail.com>
2025-07-03 17:55:37 -0700

    change internal current mod name

--- main.lua
+++ main.lua
@@ -3,22 +3,22 @@ to_big = to_big or function(x)
   return x
 end
 
-this_mod = SMODS.current_mod
+divine_intervention = SMODS.current_mod
 
 SMODS.Sound({key = "jesus", path = "jesus.ogg",})
 
-this_mod.seconds = 0
+divine_intervention.seconds = 0
 
 local updatehook = Game.update
 
 function Game:update(dt)
     updatehook(self, dt)
     if G.GAME.current_round.hands_left and G.GAME.current_round.hands_left == 1 and G.GAME.current_round.discards_left < 2 and (to_big(G.GAME.chips)) < (to_big(G.GAME.blind.chips / 1.5)) then
-        this_mod.seconds = this_mod.seconds - dt
-        if this_mod.seconds <= 0 then
-            this_mod.seconds = 2.5
+        divine_intervention.seconds = divine_intervention.seconds - dt
+        if divine_intervention.seconds <= 0 then
+            divine_intervention.seconds = 2.5
             play_sound("divi_jesus")
-            this_mod.jesuspng = load_image("jesus"..(math.random(4)-1)..".png")
+            divine_intervention.jesuspng = load_image("jesus"..(math.random(4)-1)..".png")
         end
     end
 end
@@ -27,7 +27,7 @@ local drawhook = love.draw
 function love.draw()
     drawhook()
     function load_image(fn)
-        local full_path = (this_mod.path 
+        local full_path = (divine_intervention.path 
         .. "customimages/" .. fn)
         local file_data = assert(NFS.newFileData(full_path))
         local tempimagedata = assert(love.image.newImageData(file_data))
@@ -38,8 +38,8 @@ function love.draw()
     local _yscale = love.graphics.getHeight()/1080
     
     if G.GAME.current_round.hands_left and G.GAME.current_round.hands_left == 1 and G.GAME.current_round.discards_left < 2 and (to_big(G.GAME.chips)) < (to_big(G.GAME.blind.chips / 1.5)) then
-        if this_mod.jesuspng == nil then this_mod.jesuspng = load_image("jesus0.png") end
-        love.graphics.setColor(1, 1, 1, 0.2 + (this_mod.seconds / 3.25))
-        love.graphics.draw(this_mod.jesuspng,0,0,0,_xscale,_yscale)
+        if divine_intervention.jesuspng == nil then divine_intervention.jesuspng = load_image("jesus0.png") end
+        love.graphics.setColor(1, 1, 1, 0.2 + (divine_intervention.seconds / 3.25))
+        love.graphics.draw(divine_intervention.jesuspng,0,0,0,_xscale,_yscale)
     end
 end
